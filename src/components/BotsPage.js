2baa218c8f340ea358794f600bf6d984b25bddc1
Samuel Kinyua <samwakash@gmail.com>
2024-09-04 13:03:00 +0300

    deliverables2

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -3,31 +3,38 @@ import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
+  // State for the list of all bots
   const [bots, setBots] = useState([]);
+  
+  // State for the list of bots in your army
   const [botArmy, setBotArmy] = useState([]);
 
+  // Fetch bots from the backend API
   useEffect(() => {
     fetch("http://localhost:8002/bots")
-      .then((response) => response.json())
-      .then((data) => setBots(data));
+      .then(response => response.json())
+      .then(data => setBots(data));
   }, []);
 
+  // Function to add a bot to the army
   function addBotToArmy(bot) {
     if (!botArmy.includes(bot)) {
       setBotArmy([...botArmy, bot]);
     }
   }
 
+  // Function to remove a bot from the army
   function removeBotFromArmy(bot) {
     setBotArmy(botArmy.filter((b) => b.id !== bot.id));
   }
 
+  // Function to discharge (delete) a bot permanently
   function dischargeBot(bot) {
     fetch(`http://localhost:8002/bots/${bot.id}`, {
       method: "DELETE",
     }).then(() => {
-      setBotArmy(botArmy.filter((b) => b.id !== bot.id));
       setBots(bots.filter((b) => b.id !== bot.id));
+      setBotArmy(botArmy.filter((b) => b.id !== bot.id));
     });
   }
 
@@ -38,5 +45,4 @@ function BotsPage() {
     </div>
   );
 }
-
 export default BotsPage;
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,18 +1,28 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
-  //your bot army code here...
 
+function YourBotArmy({ botArmy, removeBot }) {
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+          {botArmy.length > 0 ? (
+            botArmy.map((bot) => (
+              <BotCard 
+                key={bot.id} 
+                bot={bot} 
+                addBotToArmy={removeBot} 
+              />
+            ))
+          ) : (
+            <div>Your Bot Army is empty</div>
+          )}
         </div>
       </div>
     </div>
   );
 }
+
 
 export default YourBotArmy;
